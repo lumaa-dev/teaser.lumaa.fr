ff685cc7fcd90faa89828146c1e6362da5393f8a
Lumaa <nathan@ama49.fr>
2023-07-21 16:15:35 +0200

    refactored

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-function countdown(querySelector = "p") {
-    var countDownDate = new Date("Jul 11, 2023 15:30:00").getTime();
-
+function countdown(querySelector = "p", countdown = new Date("July 21st, 2023 21:30:00"), endString = "") {
     var x = setInterval(function() {
+        var now = new Date().getTime();
 
-    var now = new Date().getTime();
+        var distance = countdown - now;
 
-    var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let dString = days > 0 ? `${zeroify(days)}d` : ""
+        let hString = hours > 0 ? `${zeroify(hours)}h` : ""
+        // let mString = minutes > 0 ? `${minutes}m` : ""
+        // let sString = seconds > 0 ? `${seconds}s` : ""
 
-    let dString = days > 0 ? `${zeroify(days)}d` : ""
-    let hString = hours > 0 ? `${zeroify(hours)}h` : ""
-    // let mString = minutes > 0 ? `${minutes}m` : ""
-    // let sString = seconds > 0 ? `${seconds}s` : ""
+        document.querySelector(querySelector).innerText = `${dString} ${hString} ${zeroify(minutes)}m ${zeroify(seconds)}s`
 
-    document.querySelector(querySelector).innerText = `${dString} ${hString} ${zeroify(minutes)}m ${zeroify(seconds)}s`
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.querySelector(querySelector).innerText = "https://youtu.be/TdGhZpvR7CY";
-    }
+        if (distance < 0) {
+            clearInterval(x);
+            document.querySelector(querySelector).innerText = endString;
+        }
     }, 1000);
 }
 
@@ -34,4 +31,4 @@ function zeroify(int = 1) {
     }
 }
 
-countdown("p#counter");
+countdown(".counter", new Date("Jul 21st, 2023 21:30:00"), "Started.")
